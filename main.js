5a6ad7a28b127fe6efc35900d2295e8bfbc4ee14
hamzanawaz123 <hamzanawazbwp@gmail.com>
2020-10-05 23:37:20 +0500

    add my new files

---
+++ main.js
@@ -0,0 +1,34 @@
+//Navbar toggle function
+let menuToggler = document.querySelector(".menu-toggle");
+let body = document.querySelector("body");
+menuToggler.addEventListener("click", () => {
+  body.classList.toggle("open");
+});
+
+// Scroll reveal
+window.sr = ScrollReveal();
+sr.reveal(".animate-left", {
+  origin: "left",
+  duration: 1000,
+  distance: "25rem",
+  delay: 300,
+});
+
+sr.reveal(".animate-right", {
+  origin: "right",
+  duration: 1000,
+  distance: "25rem",
+  delay: 600,
+});
+sr.reveal(".animate-top", {
+  origin: "top",
+  duration: 1000,
+  distance: "25rem",
+  delay: 600,
+});
+sr.reveal(".animate-bottom", {
+  origin: "bottom",
+  duration: 1000,
+  distance: "25rem",
+  delay: 600,
+});
